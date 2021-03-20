2f254ba4811e1e04a1ee180ff48dafe5c60b0dc4
matrizlab <info@matrizlab.com>
2021-03-20 13:07:32 +0100

    Supporting Mobile Devices

--- src/posts/posts.js
+++ src/posts/posts.js
@@ -11,8 +11,10 @@ import {
     ReferenceInput,
     SelectInput,
     TextInput,
-    Filter
+    Filter,
+    SimpleList
 } from 'react-admin';
+import { useMediaQuery } from '@material-ui/core';
 
 const PostTitle = ({ record }) => {
        return <span>Post {record ? `"${record.title}"` : ''}</span>;
@@ -27,18 +29,30 @@ const PostFilter = (props) => (
     </Filter>
 );
 
-export const PostList = props => (
-    <List filters={<PostFilter />} {...props}>
-        <Datagrid>
-            <TextField source="id" />
-            <ReferenceField source="userId" reference="users">
-                <TextField source="name" />
-            </ReferenceField>
-            <TextField source="title" />
-            <EditButton />
-        </Datagrid>
-    </List>
-);
+export const PostList = (props) => {
+    const isSmall = useMediaQuery(theme => theme.breakpoints.down('sm'));
+    return (
+        <List {...props}>
+            {isSmall ? (
+                <SimpleList
+                    primaryText={record => record.title}
+                    secondaryText={record => `${record.views} views`}
+                    tertiaryText={record => new Date(record.published_at).toLocaleDateString()}
+                />
+            ) : (
+                <Datagrid>
+                    <TextField source="id" />
+                    <ReferenceField label="User" source="userId" reference="users">
+                        <TextField source="name" />
+                    </ReferenceField>
+                    <TextField source="title" />
+                    <TextField source="body" />
+                    <EditButton />
+                </Datagrid>
+            )}
+        </List>
+    );
+}
 
 export const PostEdit = props => (
     <Edit title={<PostTitle />} {...props}>
