1634808c3c7a0eb922528cefc2d80682e7b7ee84
matrizlab <info@matrizlab.com>
2021-03-20 12:06:14 +0100

    Selecting Columns

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import * as React from "react";
-import { Admin, Resource, ListGuesser } from 'react-admin';
+import { Admin, Resource } from 'react-admin';
+import { UserList } from "./users/users";
 import jsonServerProvider from 'ra-data-json-server';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
     <Admin dataProvider={dataProvider}>
-      <Resource name="users" list={ListGuesser} />
+      <Resource name="users" list={UserList} />
     </Admin>
 );
 
