adca3bfa0fa0107740717e495198d4813c8a1e0b
matrizlab <info@matrizlab.com>
2021-03-20 12:41:34 +0100

    Adding Creation and Editing Capabilities

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import * as React from "react";
-import { Admin, Resource } from 'react-admin';
+import { Admin, Resource, EditGuesser } from 'react-admin';
 import { UserList } from "./users/users";
-import { PostList } from "./posts/posts";
+import { PostList, PostEdit, PostCreate } from "./posts/posts";
 import jsonServerProvider from 'ra-data-json-server';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
     <Admin dataProvider={dataProvider}>
       <Resource name="users" list={UserList} />
-      <Resource name="posts" list={PostList} />
+      <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate}/>
     </Admin>
 );
 
--- src/posts/posts.js
+++ src/posts/posts.js
@@ -1,5 +1,17 @@
 import * as React from "react";
-import { List, Datagrid, TextField, ReferenceField, EditButton } from 'react-admin';
+import {
+    List,
+    Datagrid,
+    TextField,
+    ReferenceField,
+    EditButton,
+    Edit,
+    Create,
+    SimpleForm,
+    ReferenceInput,
+    SelectInput,
+    TextInput,
+} from 'react-admin';
 
 export const PostList = props => (
     <List {...props}>
@@ -12,4 +24,29 @@ export const PostList = props => (
             <EditButton />
         </Datagrid>
     </List>
+);
+
+export const PostEdit = props => (
+    <Edit {...props}>
+        <SimpleForm>
+            <TextInput disabled source="id" />
+            <ReferenceInput source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <TextInput multiline source="body" />
+        </SimpleForm>
+    </Edit>
+);
+
+export const PostCreate = props => (
+    <Create {...props}>
+        <SimpleForm>
+            <ReferenceInput source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <TextInput multiline source="body" />
+        </SimpleForm>
+    </Create>
 );
